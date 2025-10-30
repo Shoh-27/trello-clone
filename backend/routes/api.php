4b2bfdab0f47d3c2a0fc3d06d48dffd7fab0c293
Shohrux <shohruxnizomov170@gmail.com>
2025-10-30 21:15:17 +0500

    api.php route sozlandi va authcontrollerdagi funklar un yozildi

---
+++ backend/routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+
+// Public routes
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+// Protected routes (require authentication)
+Route::middleware('auth:sanctum')->group(function () {
+    // Auth
+    Route::get('/auth/me', [AuthController::class, 'me']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+});
