b6762071a24f327ec8bb2b2854bf6131ab0a0749
Shohrux <shohruxnizomov170@gmail.com>
2025-10-31 16:02:32 +0500

    yangi board qoshish un store metodi yozildi

--- backend/app/Http/Controllers/BoardController.php
+++ backend/app/Http/Controllers/BoardController.php
@@ -17,4 +17,29 @@ class BoardController extends Controller
 
         return response()->json($boards);
     }
+
+    /**
+     * Store a newly created board
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'background_color' => 'nullable|string|max:7',
+        ]);
+
+        $board = $request->user()->boards()->create($request->all());
+
+        // Log activity
+        ActivityLog::create([
+            'action' => 'created',
+            'description' => "{$request->user()->name} created board \"{$board->title}\"",
+            'loggable_type' => Board::class,
+            'loggable_id' => $board->id,
+            'user_id' => $request->user()->id,
+        ]);
+
+        return response()->json($board, 201);
+    }
 }
