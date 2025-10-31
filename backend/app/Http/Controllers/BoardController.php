53309aaf04ce8fae7a3dd9c9b834b98d1e618035
Shohrux <shohruxnizomov170@gmail.com>
2025-10-31 16:00:35 +0500

    boardlarni korish un index funki yozildi

--- backend/app/Http/Controllers/BoardController.php
+++ backend/app/Http/Controllers/BoardController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Board;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class BoardController extends Controller
 {
-    //
+    /**
+     * Display a listing of user's boards
+     */
+    public function index(Request $request)
+    {
+        $boards = $request->user()->boards()->with('lists.cards')->get();
+
+        return response()->json($boards);
+    }
 }
