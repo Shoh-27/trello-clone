f2144038ce8d8f634ea8bee4888dba5f691df23d
Shohrux <shohruxnizomov170@gmail.com>
2025-10-31 22:07:44 +0500

    listlar un migration qoshildi

---
+++ backend/database/migrations/2025_10_31_170702_create_lists_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('position')->default(0);
+            $table->foreignId('board_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+
+            $table->index(['board_id', 'position']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('lists');
+    }
+};
