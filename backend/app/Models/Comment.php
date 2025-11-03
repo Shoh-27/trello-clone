15476293e1e7a79b29321bb75a3c5f4871fb17e3
Shohrux <shohruxnizomov170@gmail.com>
2025-11-03 08:49:16 +0500

    backendda commentlar bn ishlashni yaxshilash un model qoshidim

--- backend/app/Models/Comment.php
+++ backend/app/Models/Comment.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'card_id',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    protected $with = ['user']; // Eager load user
+
+    // Relationships
+    public function card()
+    {
+        return $this->belongsTo(Card::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
