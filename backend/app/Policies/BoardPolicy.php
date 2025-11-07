7a4e7e88b40052ea1cdd800416baceeeafbee538
Shohrux <shohruxnizomov170@gmail.com>
2025-11-07 22:46:18 +0500

    boardlani faqat qoshgan odam ochirishi un policy yozildi

--- backend/app/Policies/BoardPolicy.php
+++ backend/app/Policies/BoardPolicy.php
@@ -2,15 +2,32 @@
 
 namespace App\Policies;
 
+use App\Models\Board;
 use App\Models\User;
 
 class BoardPolicy
 {
     /**
-     * Create a new policy instance.
+     * Determine if the user can view the board.
      */
-    public function __construct()
+    public function view(User $user, Board $board): bool
     {
-        //
+        return $board->user_id === $user->id;
+    }
+
+    /**
+     * Determine if the user can update the board.
+     */
+    public function update(User $user, Board $board): bool
+    {
+        return $board->user_id === $user->id;
+    }
+
+    /**
+     * Determine if the user can delete the board.
+     */
+    public function delete(User $user, Board $board): bool
+    {
+        return $board->user_id === $user->id;
     }
 }
